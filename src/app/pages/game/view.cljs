9e7194042045c1ef2864545439a72a21d7f42a53
Georgii Ivannikov <ivannikogeorge1337@gmail.com>
2021-12-15 22:14:59 +0300

    More beautiful icons

--- src/app/pages/game/view.cljs
+++ src/app/pages/game/view.cljs
@@ -67,11 +67,11 @@
      [:div [:span.pt-mn [:img.pt-icn {:src "./img/coin_gold.png"}]
             (:balance pt) "/20"]]]]
    [:div.pt-stats
-    [:div [:span [:img.pt-icn {:src "./img/temp.png"}]       "5/5 temperature"]]
-    [:div [:span [:img.pt-icn {:src "./img/potion_red.png"}] "3/5 pressure"]]
-    [:div [:span [:img.pt-icn {:src "./img/sugar.png"}]      "2/5 sugar"]]
-    [:div [:span [:img.pt-icn {:src "./img/orc_green.png"}]  "2/5 bacteria"]]
-    [:div [:span [:img.pt-icn {:src "./img/coin_gold.png"}]  "4/5 diarea"]]]])
+    [:div [:span [:img.pt-icn {:src "./img/thermometer.png"}]   "5/5 temperature"]]
+    [:div [:span [:img.pt-icn {:src "./img/tonometer.png"}] "3/5 pressure"]]
+    [:div [:span [:img.pt-icn {:src "./img/sugar.png"}]         "2/5 sugar"]]
+    [:div [:span [:img.pt-icn {:src "./img/orc_green.png"}]     "2/5 bacteria"]]
+    [:div [:span [:img.pt-icn {:src "./img/diarrhea.png"}]      "4/5 diarea"]]]])
 
 (defn plusify [t] (if (> t 0 ) (str "+" t) t))
 
@@ -83,17 +83,17 @@
      [:div.rpgui-icon.empty-slot.tabl-ico
       [:img.med-img {:src (:img m)}]]
      [:div.grow-1
-
       (when-let [t (get-in m [:effects :temperature])]
-        [:div [:span [:img.pt-icn {:src "./img/temp.png"}] (plusify t)]])
-      (when-let [t (get-in m [:effects :pressure])]
-        [:div [:span [:img.pt-icn {:src "./img/potion_red.png"}] (plusify t)]])
-      (when-let [t (get-in m [:effects :sugar])]
-        [:div [:span [:img.pt-icn {:src "./img/sugar.png"}] (plusify t)]])
-      (when-let [t (get-in m [:effects :bacteria])]
-        [:div [:span [:img.pt-icn {:src "./img/orc_green.png"}] (plusify t)]])
-      (when-let [t (get-in m [:effects :diarea])]
-        [:div [:span [:img.pt-icn {:src "./img/coin_gold.png"}] (plusify t)]])
+       [:div [:span [:img.pt-icn {:src "./img/thermometer.png"}] (plusify t)]])
+     (when-let [t (get-in m [:effects :pressure])]
+       [:div [:span [:img.pt-icn {:src "./img/tonometer.png"}] (plusify t)]])
+     (when-let [t (get-in m [:effects :sugar])]
+       [:div [:span [:img.pt-icn {:src "./img/sugar.png"}] (plusify t)]])
+     (when-let [t (get-in m [:effects :bacteria])]
+       [:div [:span [:img.pt-icn {:src "./img/orc_green.png"}] (plusify t)]])
+     (when-let [t (get-in m [:effects :diarea])]
+       [:div [:span [:img.pt-icn {:src "./img/diarrhea.png"}] (plusify t)]])
+
       ]]]])
 
 (defn aidbox [med]
